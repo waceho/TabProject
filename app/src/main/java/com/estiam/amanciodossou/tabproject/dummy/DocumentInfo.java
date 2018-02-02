164d10433560852a9294a876d540c354e6a5a292
waceho <amancio.perros@gmail.com>
2018-02-03 00:12:30 +0100

    delete Dummy fake data
    delete Dummy fake data

--- app/src/main/java/com/estiam/amanciodossou/tabproject/dummy/DocumentInfo.java
+++ app/src/main/java/com/estiam/amanciodossou/tabproject/dummy/DocumentInfo.java
@@ -36,32 +36,6 @@ public final class DocumentInfo implements Parcelable {
         return mTitle;
     }
 
-    public List<ModuleInfo> getModules() {
-        return mModules;
-    }
-
-    public boolean[] getModulesCompletionStatus() {
-        boolean[] status = new boolean[mModules.size()];
-
-        for(int i=0; i < mModules.size(); i++)
-            status[i] = mModules.get(i).isComplete();
-
-        return status;
-    }
-
-    public void setModulesCompletionStatus(boolean[] status) {
-        for(int i=0; i < mModules.size(); i++)
-            mModules.get(i).setComplete(status[i]);
-    }
-
-    public ModuleInfo getModule(String moduleId) {
-        for(ModuleInfo moduleInfo: mModules) {
-            if(moduleId.equals(moduleInfo.getModuleId()))
-                return moduleInfo;
-        }
-        return null;
-    }
-
     @Override
     public String toString() {
         return mTitle;
